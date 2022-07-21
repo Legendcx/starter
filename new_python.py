366a335cbff8bb565037a126f3631b88cca183a8
Legendcx <108401768+Legendcx@users.noreply.github.com>
2022-07-21 08:35:37 +0300

    Update new_python.py

--- new_python.py
+++ new_python.py
@@ -5,3 +5,22 @@ for i in a:
 
 print(True and False or True or not True)
 
+#  CLASS oluşturma
+
+class calisan:
+  def __init__(self, name, surname, age):
+    self.name = name
+    self.surname = surname
+    self.age = age
+calisan1 = calisan("Ali", "Yazar", 42)
+print(calisan1.name, calisan1.surname, calisan1.age)
+
+##  ŞİMDİ CLASS IMIZA METOD EKLEYCEĞİZ.  ----
+
+def show_info(self):
+  print(f"Ad : {self.namw} Soyad : {self.surname} Yaş : {self.age})
+      
+calisan1.show_info()
+        
+calisan.show_info(calisan1)  # BU ŞEKİLDE DE ÇAĞIRABİLİRİZ.
+
